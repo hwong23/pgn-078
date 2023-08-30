adaef197f780cba7e793b5cb73db41146275c913
harry.wong <hwong23@gmail.com>
2023-08-30 15:37:12 -0500

    upd

---
+++ archimd.sh
@@ -0,0 +1,12 @@
+# git checkout output
+# git pull
+
+cp $1/02n.a1.doc.md content/
+cp $1/*.png content/images/
+
+# f="`ls -t ~/Downloads/tmpr/*.docx | head -1`"
+# basedocxFILE=${f##*/};  
+# open $basedocxFILE
+
+# git checkout main
+
