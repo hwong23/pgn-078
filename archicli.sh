4789c9d3a874ea7cb4caae8ae84e628c2e3ae984
hwo <hwong23@gmail.com>
2023-09-14 22:15:31 -0500

    args

---
+++ archicli.sh
@@ -0,0 +1,3 @@
+/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel /Users/hwo/gh/model/pgn-078-arq --script.runScript /Users/hwo/gh/jarchi-hwo/hwo/markdownDrivingViews-devdocs2.ajs -myArg "HelloWord"
+
+# /Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel /Users/hwo/gh/model/pgn-078-arq --script.runScript /Users/hwo/gh/jarchi-hwo/hwo/markdownDrivingViews-devdocs2.ajs
