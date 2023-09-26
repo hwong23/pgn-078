15465f9f155ac26f8a1d3534cd2fa19d1270727f
harry.wong <hwong23@gmail.com>
2023-09-26 15:33:19 -0500

    sh

--- archicli.sh
+++ archicli.sh
@@ -1,1 +1,3 @@
-/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel /Users/hwo/gh/model/pgn-078-arq --script.runScript /Users/hwo/gh/jarchi-hwo/hwo/$1 -vistaDocumental $2
+#/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel $1/$2/pgn-078-arq $1/jarchi-hwo/hwo/$3 -vistaDocumental $4
+
+/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel $1/$2/pgn-078-arq --script.runScript $1/jarchi-hwo/hwo/$3 -vistaDocumental $4
