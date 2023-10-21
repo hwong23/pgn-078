583d18b8604546d0601e7fdd77081656696457b0
hwo <hwong23@gmail.com>
2023-10-21 13:50:50 -0500

    histr19

--- build/build.sh
+++ build/build.sh
@@ -27,9 +27,9 @@ COMMIT="${TRIGGERING_SHA_7}"
 # Pandoc's configuration is specified via files of option defaults
 # located in the $PANDOC_DATA_DIR/defaults directory.
 PANDOC_DATA_DIR="${PANDOC_DATA_DIR:-build/pandoc}"
-HISTORIAMD="$(git show -s -n 4 --format='| 1.%h | %s |' --abbrev-commit HEAD)"
+HISTORIAMD="${git show -s -n 4 --format='| 1.%h | %s |' --abbrev-commit HEAD}"
 export FECHA_COMPILACION COMMIT PROYECTO PROY_DESCR
-echo $HISTORIAMD
+echo "HV " $HISTORIAMD
 
 # Generate reference information
 echo >&2 "Retrieving and processing reference metadata"
@@ -103,7 +103,7 @@ if [ "${BUILD_DOCX}" = "true" ]; then
     envsubst < "$f" > mdfile.hash
     mv mdfile.hash "$f"
 
-    echo "sustituya < $f > $f.hash"
+    echo "sustituye < $f > $f.hash"
 
     pandoc --verbose \
       --data-dir="$PANDOC_DATA_DIR" \
