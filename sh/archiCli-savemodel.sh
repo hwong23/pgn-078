2cc88c7571a5b45f42e099786e6117479b485b92
hwo <hwong23@gmail.com>
2024-05-29 06:59:07 -0500

    cambio

--- sh/archiCli-savemodel.sh
+++
@@ -1,7 +0,0 @@
-# $1: ruta usuario repositorio git
-# $2: ruta y modelo arq
-# $3: ruta programa ajs
-
-
-/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel $1/$2 --script.runScript $1/jarchi-hwo/examples/$3 -vistaDocumental $4
-
--- sh/cpush.sh
+++
@@ -1,8 +0,0 @@
-# $1: ruta repo git origen
-
-cd $1
-
-git add .
-git commit -a -m "$2"
-git push
-git pull
